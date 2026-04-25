9c3e08f8388a9e10d86a6bcea5b51244bbd6c258
saitejadst <saitejad427@gmail.com>
2026-04-25 12:17:27 +0530

    variableexample

---
+++ Day-03/test.py
@@ -0,0 +1,17 @@
+ec2_instance_name = "saiteja_xyz"
+
+print("EC2 Instance Name:", ec2_instance_name)
+
+a = 10
+b = 5
+
+def addition():
+    print(a + b)
+
+def sub():
+    print(a - b)
+
+addition()
+sub()
+
+
